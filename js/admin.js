e6342e8645f6c035f363da03c9c95f35453486e9
´Peco´ <pecohernandez6@gmail.com>
2022-10-27 10:13:22 -0300

    realice la paguina admin

--- js/admin.js
+++ js/admin.js
@@ -1,93 +1,177 @@
-import Productos from "./productos.js";
+import Producto from "./productos.js";
 
-let listaDeProductos =
-  JSON.parse(localStorage.getItem("listaProductoskey")) || [];
+let listaProductos =
+  JSON.parse(localStorage.getItem("listaProductosKey")) || [];
 
 const modalFormProducto = new bootstrap.Modal(
   document.querySelector("#modalProducto")
 );
-
 const btnCrearProducto = document.querySelector("#btnCrearProducto");
 let codigo = document.querySelector("#codigo");
-let nombre = document.querySelector("#nombre");
+let modelo = document.querySelector("#modelo");
 let descripcion = document.querySelector("#descripcion");
 let imagen = document.querySelector("#imagen");
 let precio = document.querySelector("#precio");
-let marca = document.querySelector("#marca");
 let formulario = document.querySelector("#formProducto");
+let productoNuevo = true;
 
-btnCrearProducto.addEventListener("click", mostarformulario);
+btnCrearProducto.addEventListener("click", mostrarFormulario);
 formulario.addEventListener("submit", crearProducto);
 
 cargaInicial();
 
 function cargaInicial() {
-  if (listaDeProductos.length > 0) {
-    listaDeProductos.map((producto) => {
-      crearfila(producto);
+  if (listaProductos.length > 0) {
+    listaProductos.map((producto) => {
+      crearFila(producto);
     });
   }
 }
-function crearfila(producto) {
-  //console.log(producto)
-  let tablaProductos = document.querySelector("#tablaProductos");
-  tablaProductos.innerHTML += `<tr>
-  <th scope="row">${producto.codigo}</th>
-  <td>${producto.nombre}</td>
-  <td>${producto.descripcion}</td>
-  <td>${producto.imagen}</td>
-  <td>${producto.precio}</td>
-  <td>${producto.marca}</td>
-  <td>
-    <button class="btn btn-warning" onclick="editarPelicula('${producto.codigo}')">
-      <i class="bi bi-pencil-square"></i>
-    </button>
-    <button class="btn btn-danger" onclick="borrarPelicula('${producto.codigo}')">
-      <i class="bi bi-x-square"></i>
-    </button>
-  </td>
-</tr>`
+
+function crearFila(producto) {
+  let tablaProducto = document.querySelector("#tablaProducto");
+  tablaProducto.innerHTML += `<tr>
+    <th scope="row">${producto.codigo}</th>
+    <td>${producto.modelo}</td>
+    <td>${producto.descripcion}</td>
+    <td>${producto.imagen}</td>
+    <td>${producto.precio}</td>
+    <td>
+      <button class="btn btn-warning" onclick="editarProducto('${producto.codigo}')">
+        <i class="bi bi-pencil-square"></i>
+      </button>
+      <button class="btn btn-danger" onclick="borrarProducto('${producto.codigo}')">
+        <i class="bi bi-x-square"></i>
+      </button>
+    </td>
+  </tr>`;
 }
 
-function mostarformulario() {
+function mostrarFormulario() {
+  productoNuevo = true;
+
+  limpiarFormulario();
   modalFormProducto.show();
   codigo.value = uuidv4();
 }
 
 function crearProducto(e) {
   e.preventDefault();
-  //agregar validaciones
-  //crear un producto
-  const nuevoProducto = new Productos(
+
+  if (productoNuevo) {
+    generarProducto();
+  } else {
+    actualizarProducto();
+  }
+}
+
+function generarProducto() {
+  //crear una pelicula
+  const nuevaProducto = new Producto(
     codigo.value,
-    nombre.value,
+    modelo.value,
     descripcion.value,
     imagen.value,
-    precio.value,
-    marca.value
+    precio.value
   );
-  //console.log(nuevoProducto);
-  //guardar un producto
-  listaDeProductos.push(nuevoProducto);
-  //console.log(listaDeProductos);
-  //guardar datoen el local
-  guadarDatosEnLS();
-  //limpiar el formulario
+
+  listaProductos.push(nuevaProducto);
+
+  guardarDatosEnLS();
+
   limpiarFormulario();
-  //dibujar esta peli en la tabla
-  crearfila(nuevoProducto)
-  //cerar ventana modal
+
+  crearFila(nuevaProducto);
+
+  Swal.fire(
+    "Producto creado",
+    "El Producto fue creada correctamente",
+    "success"
+  );
+
   modalFormProducto.hide();
 }
+
 function limpiarFormulario() {
   formulario.reset();
-  //resetear la clase de bootstrap form-control
-  nombre.className = "form-control";
+
+  modelo.className = "form-control";
   descripcion.className = "form-control";
   imagen.className = "form-control";
   precio.className = "form-control";
-  marca.className = "form-control";
+  codigo.className = "form-control";
 }
-function guadarDatosEnLS() {
-  localStorage.setItem("listaProductoskey", JSON.stringify(listaDeProductos));
+
+function guardarDatosEnLS() {
+  localStorage.setItem("listaProductosKey", JSON.stringify(listaProductos));
+}
+
+window.borrarProducto = function (codigo) {
+  Swal.fire({
+    title: "Eliminar Producto",
+    text: "Esta por eliminar el producto seleccionada, no puede revertir este paso",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Borrar",
+    cancelButtonText: "Cancelar",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      let copiaListaProductos = listaProductos.filter(
+        (producto) => producto.codigo != codigo
+      );
+      listaProductos = copiaListaProductos;
+
+      guardarDatosEnLS();
+
+      actualizarTabla();
+      Swal.fire(
+        "Producto eliminado",
+        "El producto seleccionada se borro correctamente",
+        "success"
+      );
+    }
+  });
+};
+
+function actualizarTabla() {
+  let tablaProducto = document.querySelector("#tablaProducto");
+  tablaProducto.innerHTML = "";
+  cargaInicial();
+}
+
+window.editarProducto = function (codigoBuscado) {
+  productoNuevo = false;
+
+  modalFormProducto.show();
+
+  let proBuscado = listaProductos.find(
+    (producto) => producto.codigo === codigoBuscado
+  );
+
+  codigo.value = proBuscado.codigo;
+  modelo.value = proBuscado.modelo;
+  descripcion.value = proBuscado.descripcion;
+  imagen.value = proBuscado.imagen;
+  precio.value = proBuscado.precio;
+};
+
+function actualizarProducto() {
+  let posicionProducto = listaProductos.findIndex(
+    (producto) => producto.codigo === codigo.value
+  );
+
+  listaProductos[posicionProducto].modelo = modelo.value;
+  listaProductos[posicionProducto].descripcion = descripcion.value;
+  listaProductos[posicionProducto].imagen = imagen.value;
+  listaProductos[posicionProducto].precio = precio.value;
+
+  guardarDatosEnLS();
+
+  actualizarTabla();
+
+  modalFormProducto.hide();
+
+  limpiarFormulario();
 }
--- js/productos.js
+++ js/productos.js
@@ -1,10 +1,10 @@
-export default class Productos {
-  construtor(codigo, nombre, descripcion, imagen, precio, marca) {
-    this.codigo = codigo;
-    this.nombre = nombre;
-    this.descripcion = descripcion;
-    this.imagen = imagen;
-    this.precio = precio;
-    this.marca = marca;
+export default class Producto{
+  constructor(codigo, modelo, descripcion, imagen, precio){
+      this.codigo = codigo;
+      this.modelo = modelo;
+      this.descripcion = descripcion;
+      this.imagen = imagen;
+      this.precio = precio;
   }
+  
 }
